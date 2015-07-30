c850d5a0732acc2b2dcd860209a5cc2adc822e42
João Mello <joao.mello@uniriotec.br>
2015-07-30 08:12:33 -0300

    First tokenizer

---
+++ rcss/tokenizer.rb
@@ -0,0 +1,27 @@
+module RJSON
+  class Tokenizer
+    SELECTOR  = /([.#][a-zA-Z][^{]{1,})/ix
+    PROPERTY  = /[^{}](.+?):(.+?);[^{}]/ix
+    BEGINRULE = /{/x
+    ENDRULE   = /}/x
+
+    def initialize io
+      @ss = StringScanner.new io.read
+    end
+
+    def next_token
+      return if @ss.eos?
+
+      case
+      when text = @ss.scan(SELECTOR)   then [:SELECTOR, text]
+      when text = @ss.scan(BEGINRULE)  then [:BEGINRULE, text]
+      when text = @ss.scan(PROPERTY)   then [:PROPERTY, text]
+      when text = @ss.scan(ENDRULE)    then [:ENDRULE, text]
+
+      else
+        x = @ss.getch
+        [x, x]
+      end
+    end
+  end
+end
